ac9d9c66cadcae2e2235987dbc2fcaa3f011a0d9
Christian Perez <nesd4rkfocus@gmail.com>
2023-05-02 16:42:29 -0300

    Added GET Method

--- first-project/src/server.js
+++ first-project/src/server.js
@@ -3,5 +3,9 @@ const express = require('express');
 // Initialize express
 const app = express();
 
+app.get('/sayHello', (request, response) => {
+  response.send('Hello, world!');
+});
+
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
